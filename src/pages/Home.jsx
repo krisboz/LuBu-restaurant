35377c4c665346f592d95601b1229b475f3c62eb
krisboz <kristijan.bozinovic1@gmail.com>
2023-10-20 12:34:14 +0200

    Improved homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
+import heroPhoto from "../assets/hero-photo.png";
 import "../styles/Home.scss";
 
 const Home = () => {
@@ -8,11 +9,23 @@ const Home = () => {
   return (
     <main className="home">
       <h1>{t("hero-headline")}</h1>
-      <p>{t("hero-description")}</p>
 
-      <div className="cta-container">
-        <Link to={"/menu"}>{t("hero-cta-menu")}</Link>{" "}
-        <Link>{t("hero-cta-reserve")}</Link>{" "}
+      <div className="hero-container">
+        <div className="description-container">
+          <p>{t("hero-description")}</p>
+
+          <div className="cta-container">
+            <Link to={"/menu"}>{t("hero-cta-menu")}</Link>{" "}
+            <Link>{t("hero-cta-reserve")}</Link>{" "}
+          </div>
+        </div>
+        <div className="photo-container">
+          <img
+            src={heroPhoto}
+            alt="picture of delicious sushi food"
+            loading="lazy"
+          ></img>
+        </div>
       </div>
     </main>
   );
